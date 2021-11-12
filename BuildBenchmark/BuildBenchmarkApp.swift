c94b00084f29d5aa18bacfbde9983ef7c85bbfde
akio0911 <akio0911@gmail.com>
2021-11-12 18:40:53 +0900

    Initial Commit

---
+++ BuildBenchmark/BuildBenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildBenchmarkApp.swift
+//  BuildBenchmark
+//
+//  Created by akio0911 on 2021/11/12.
+//
+
+import SwiftUI
+
+@main
+struct BuildBenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
